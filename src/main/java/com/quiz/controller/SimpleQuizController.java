dd0623cf76e16a4958e9efb679c773bb7bd5e2c8
Egor Polivtsev <letsbattle@yandex.ru>
2020-11-30 15:47:18 +0300

    MAP2: Added SimpleQuizController

---
+++ src/main/java/com/quiz/controller/SimpleQuizController.java
@@ -0,0 +1,14 @@
+package com.quiz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleQuizController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Quiz page";
+    }
+
+}
